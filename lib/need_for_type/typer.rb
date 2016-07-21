3264675f3c4ff9195a3c591ce3130cb67d6c29c3
tiagonbotelho <tiagonbotelho@hotmail.com>
2016-07-21 22:55:49 +0100

    starts implementing menu

--- lib/need_for_type/typer.rb
+++ lib/need_for_type/typer.rb
@@ -1,17 +1,25 @@
+require 'logger'
 require './need_for_type/window_manager'
 
 module NeedForType
   class Typer
 
     def initialize
+      @logger = Logger.new('typer.txt')
       @window_manager = WindowManager.new
     end
 
     def update
       while input_char = @window_manager.get_input
+        @logger.info(input_char)
+
+        if input_char == 27
+          @window_manager.add_close_message
+          break
+        end
         @window_manager.display_window.setpos(2,2)
         @window_manager.display_window.addstr(input_char)
-        @window_manager.display_window.refresh
+        @window_manager.refresh_display
       end
     end
 
--- lib/need_for_type/window_manager.rb
+++ lib/need_for_type/window_manager.rb
@@ -7,7 +7,6 @@ class WindowManager
     @display_window.box("|", "-")
 
     @display_window.setpos(2, 2)
-    @display_window.addstr("Hello")
     @display_window.refresh
 
     @input_window = Curses::Window.new(Curses.lines, Curses.cols, Curses.lines/1.5, 0)
@@ -17,8 +16,34 @@ class WindowManager
     @input_window.refresh
   end
 
+  def render_menu
+    @display_window.clear
+    @display_window.setpos(2,2)
+    @display_window.addstr("Need For Type")
+    @display_window.setpos(3,2)
+    @display_window.attrset(1, Curses::A_STANDOUT)
+    @display_window.addstr("1. ")
+  end
+
   def get_input
     @input_window.getch
+  end
+
+  def refresh_display
+    @display_window.refresh
+  end
+
+  def refresh_input
+    @input_window.refresh
+  end
+
+  # TODO: make this pretty
+  def add_close_message
+    @display_window.clear
+    @display_window.setpos(2,2)
+    @display_window.addstr("Thank you goodbye!")
+    @display_window.refresh
+    sleep 3
   end
 
 end

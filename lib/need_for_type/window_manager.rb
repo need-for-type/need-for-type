596984912d647360d7c2a8ee934f6d54ebff3f81
Joao Bernardo <jb.amaro95@gmail.com>
2016-07-21 20:47:22 +0100

    Display loaded text on screen

--- lib/need_for_type/window_manager.rb
+++ lib/need_for_type/window_manager.rb
@@ -3,11 +3,14 @@ require 'curses'
 class WindowManager
 
   def initialize()
+    @file_manager = FileManager.new('easy') # TODO  
+    @file_manager.load_file
+
     @window = Curses::Window.new(Curses.lines, Curses.cols, 0, 0)
     @window.box("|", "-")
 
-    @window.setpos(2, 2)
-    @window.addstr("Hello")
+    @window.setpos(1, 2)
+    @window.addstr(@file_manager.content)
 
     @window.refresh
   end
